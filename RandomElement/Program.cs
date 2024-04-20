9e86b63aca8ec3a47cbf2d3eb4796aef6aaf98f9
Morris Vaide <mvaide@tktk.ee>
2024-04-20 17:44:56 +0300

    Week6

---
+++ RandomElement/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snacks = { "sushi", "pizza", "burger", "chicken wings", "asian takeaways" };
+
+Random rnd = new Random();
+
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"tonight we're eating {snacks[randomIndex]}");
